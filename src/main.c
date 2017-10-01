faf7fa7f386bf845b378bb8893560007f34aa62d
netomenoci <jn.menoci@hotmail.com>
2017-09-30 21:37:33 -0300

    works fine

--- src/main.c
+++ src/main.c
@@ -3,12 +3,20 @@
 int main() {
   int a[5];
   int n;
+  int aux;
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
-    /* Imprime resultado da n-esima iteracao do scheduler */
-  }
-
+    for (int i=1; i<(n+1); i++) {
+        aux = 0;
+        for(int j =0; j<=4; j++){
+            if((i%a[j])==0){
+                printf("%i",j);
+                aux ++;
+            }
+        }
+        if(aux == 0) printf("-");
+        printf("\n");
+    }
   return 0;
 }
